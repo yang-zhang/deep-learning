8279f181054b66208f537cfc4feb269b2f92229c
yang-zhang <zhangyangcolostate@gmail.com>
2017-12-01 15:36:53 -0500

    Update

--- keras_time_series.py
+++ keras_time_series.py
@@ -1,65 +1,82 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[70]:
 
-from ds_utils.imports import *
-
-
-# In[133]:
-
-data = pd.read_csv(
-    'data/international-airline-passengers.csv', engine='python', skipfooter=3)
-
-
-# In[134]:
-
-plt.plot(data.iloc[:, 1])
+get_ipython().magic('matplotlib inline')
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import keras
+import sklearn
+import sklearn.preprocessing
+import sklearn.metrics
 
 
-# In[135]:
+# In[37]:
+
+url = 'https://raw.githubusercontent.com/blue-yonder/pydse/master/pydse/data/international-airline-passengers.csv'
+data = pd.read_csv(url, sep=';')
+
+
+# In[38]:
+
+data.head()
+
+
+# In[46]:
+
+plt.plot(data.Passengers)
+
+
+# In[40]:
 
 np.random.seed(7)
 
 
-# In[169]:
+# In[41]:
 
 ts = data.iloc[:, 1].astype('float').values.reshape(-1, 1)
 
 
-# In[170]:
+# In[42]:
 
 ts.shape
 
 
-# In[138]:
+# In[43]:
 
 scaler = sklearn.preprocessing.MinMaxScaler(feature_range=(0, 1))
 ts = scaler.fit_transform(ts)
 
 
-# In[139]:
+# In[47]:
+
+plt.plot(ts)
+
+
+# In[52]:
 
 train_size = int(len(ts) * 0.67)
 
 
-# In[140]:
+# In[53]:
 
 ts_train = ts[:train_size]
 ts_test = ts[train_size:]
 
 
-# In[187]:
+# In[54]:
 
 ts_train.shape
 
 
-# In[188]:
+# In[55]:
 
 ts_test.shape
 
 
-# In[142]:
+# In[56]:
 
 look_back=1
 def make_xy(ts):
@@ -68,29 +85,29 @@ def make_xy(ts):
     return X, Y
 
 
-# In[143]:
+# In[57]:
 
 X_train, Y_train = make_xy(ts_train)
 X_test, Y_test = make_xy(ts_test)
 
 
-# In[144]:
+# In[58]:
 
 X_train = X_train.reshape(X_train.shape[0], 1, 1)
 X_test = X_test.reshape(X_test.shape[0], 1, 1)
 
 
-# In[145]:
+# In[59]:
 
 X_train.shape
 
 
-# In[146]:
+# In[60]:
 
 Y_train.shape
 
 
-# In[147]:
+# In[61]:
 
 model = keras.models.Sequential([
     keras.layers.LSTM(4, input_shape=(1, look_back)),
@@ -98,64 +115,65 @@ model = keras.models.Sequential([
 ])
 
 
-# In[148]:
+# In[62]:
 
 model.compile(optimizer=keras.optimizers.Adam(), loss=keras.losses.mean_squared_error)
 
 
-# In[149]:
+# In[63]:
 
 model.fit(X_train, Y_train, epochs=100, batch_size=1, validation_data=[X_test, Y_test])
 
 
-# In[160]:
+# In[64]:
 
 pred_train = model.predict(X_train)
 pred_test = model.predict(X_test)
 
 
-# In[161]:
+# In[65]:
 
 pred_train_original_scale = scaler.inverse_transform(pred_train)
 pred_test_original_scale = scaler.inverse_transform(pred_test)
 
 
-# In[163]:
+# In[66]:
 
 Y_train_original_scale = scaler.inverse_transform(Y_train)
 Y_test_original_scale = scaler.inverse_transform(Y_test)
 
 
-# In[164]:
+# In[71]:
 
 train_rmse = np.sqrt(sklearn.metrics.mean_squared_error(Y_train_original_scale, pred_train_original_scale))
 train_rmse
 
 
-# In[165]:
+# In[72]:
 
 test_rmse = np.sqrt(sklearn.metrics.mean_squared_error(Y_test_original_scale, pred_test_original_scale))
 test_rmse
 
 
-# In[166]:
+# In[73]:
 
 pred_test.shape
 
 
-# In[167]:
+# In[74]:
 
 pred_train.shape
 
 
-# In[168]:
+# In[75]:
 
 ts.shape
 
 
-# In[185]:
+# In[80]:
 
-plt.plot(ts)
+plt.plot(data.Passengers)
+
 plt.plot(np.vstack([pred_train_original_scale, pred_test_original_scale]))
 
 
