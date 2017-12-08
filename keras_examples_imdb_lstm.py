54b9f22dbc383758f8edd071cbd49a912b01beda
yang-zhang <zhangyangcolostate@gmail.com>
2017-12-07 22:19:02 -0500

    Add polling

--- keras_examples_imdb_lstm.py
+++ keras_examples_imdb_lstm.py
@@ -3,12 +3,12 @@
 
 # https://github.com/fchollet/keras/blob/master/examples/imdb_lstm.py
 
-# In[2]:
+# In[9]:
 
 import utilities
 
 
-# In[81]:
+# In[151]:
 
 '''Trains an LSTM model on the IMDB sentiment classification task.
 The dataset is actually too small for LSTM to be of any advantage
@@ -27,6 +27,12 @@ from keras.models import Sequential
 from keras.layers import Dense, Embedding
 from keras.layers import LSTM
 from keras.datasets import imdb
+import numpy as np
+
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import log_loss, accuracy_score
+from sklearn.preprocessing import LabelEncoder
 
 max_features = 20000
 maxlen = 80  # cut texts after this number of words (among top max_features most common words)
@@ -38,7 +44,7 @@ print(len(x_train), 'train sequences')
 print(len(x_test), 'test sequences')
 
 
-# In[82]:
+# In[152]:
 
 print('Pad sequences (samples x time)')
 x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
@@ -47,7 +53,44 @@ print('x_train shape:', x_train.shape)
 print('x_test shape:', x_test.shape)
 
 
-# In[84]:
+# In[153]:
+
+word_index = imdb.get_word_index()
+index_word = dict((v, k) for k, v in word_index.items())
+
+
+# In[154]:
+
+' '.join('0' if i==0 else index_word[i] for i in x_train[np.random.choice(len(x_train))])
+
+
+# ### logistic regression
+
+# In[155]:
+
+logreg = LogisticRegression()
+logreg.fit(x_train, np.array(y_train).reshape(-1, 1))
+
+
+# In[157]:
+
+valid_probs = logreg.predict_proba(x_test)
+valid_preds = logreg.predict(x_test)
+
+
+# In[165]:
+
+log_loss(y_test, valid_probs)
+
+
+# In[160]:
+
+accuracy_score(y_test, valid_preds)
+
+
+# ### rnn
+
+# In[87]:
 
 print('Build model...')
 model = Sequential()
